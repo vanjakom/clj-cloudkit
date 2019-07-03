a7928256ba790e5c477f5234b37d0f2ec5a7d9f5
Vanja Komadinovic <vanjakom@gmail.com>
2019-07-03 09:15:24 -0700

    project update

--- project.clj
+++ project.clj
@@ -3,13 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repl-options {
-                  :nrepl-middleware
-                  [lighttable.nrepl.handler/lighttable-ops]}
-  :dependencies [
-                  [org.clojure/clojure "1.8.0"]
+   :dependencies [
                   [org.clojure/data.json "0.2.6"]
                   [clj-http "2.2.0"]
-                  [lein-light-nrepl "0.3.2"]
-
-                  [com.mungolab/clj-common "0.2.0-SNAPSHOT"]])
+ 
+                  [com.mungolab/clj-common "0.3.0-SNAPSHOT"]])
