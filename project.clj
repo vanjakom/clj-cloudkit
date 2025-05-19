3a74eacc19fd19ef2fb3c7dde7fc974292397230
Vanja Komadinovic <vanjakom@gmail.com>
2025-05-19 19:16:18 +0200

    0.1.0 using release version of dependencies, setting deploy

--- project.clj
+++ project.clj
@@ -1,10 +1,16 @@
-(defproject com.mungolab/clj-cloudkit "0.1.0-SNAPSHOT"
+(defproject
+  com.mungolab/clj-cloudkit
+  "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-   :dependencies [
-                  [org.clojure/data.json "0.2.6"]
-                  [clj-http "2.2.0"]
- 
-                  [com.mungolab/clj-common "0.3.0-SNAPSHOT"]])
+  :deploy-repositories [
+                        ["clojars" {
+                                    :url "https://clojars.org/repo"
+                                    :sign-releases false}]]  
+  :dependencies [
+                 [com.mungolab/clj-common "0.3.2"]
+                 
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "2.2.0"]])
